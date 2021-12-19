c9f9cfedd249c1b4a2fd610fbeb1d0e3e13260a8
Peter <peter.balling@gmail.com>
2021-12-19 12:03:34 +0100

    Implimented PigLatin class on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
+#include "PigLatin.h"
+
 #include <iostream>
+#include <exception>
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+
+	try
+	{
+		PigLatin pl(argv[1]);
+
+	}
+	catch (std::string& e)
+	{
+		std::cout << e << std::endl;
+		return 2;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Faild to obfuscate file: " << e.what() << std::endl;
+		return 3;
+	}
+
 	return 0;
 }
